fcccdc32c2311168c2a2fba597dfc7d6dbaba441
cw2234 <2234372218@qq.com>
2025-07-18 14:41:21 +0800

    把多线程é£删了

--- train.py
+++ train.py
@@ -101,30 +101,62 @@ def invoke_model(actor_agent, obs, exp):
     return node, use_exec
 
 
-def train_agent(agent_id, param_queue, reward_queue, adv_queue, gradient_queue):
-    # model evaluation seed
-    tf.set_random_seed(agent_id)
+def main():
+    np.random.seed(args.seed)
+    tf.set_random_seed(args.seed)
 
-    # set up environment
+    # create result and model folder
+    create_folder_if_not_exists(args.result_folder)
+    create_folder_if_not_exists(args.model_folder)
+
+    # model evaluation seed
+    tf.set_random_seed(0)
+
+
     env = Environment()
+    worker_config = tf.ConfigProto(device_count={'GPU': args.worker_num_gpu},
+                                   gpu_options=tf.GPUOptions(per_process_gpu_memory_fraction=args.worker_gpu_fraction))
+    worker_sess = tf.Session(config=worker_config)
+    worker_actor_agent = ActorAgent(worker_sess, args.node_input_dim, args.job_input_dim, args.hid_dims,
+                                    args.output_dim,
+                                    args.max_depth, range(1, args.exec_cap + 1))
+
 
     # gpu configuration
-    config = tf.ConfigProto(device_count={'GPU': args.worker_num_gpu},
-                            gpu_options=tf.GPUOptions(per_process_gpu_memory_fraction=args.worker_gpu_fraction))
-
-    sess = tf.Session(config=config)
-
+    main_config = tf.ConfigProto(device_count={'GPU': args.master_num_gpu},
+                            gpu_options=tf.GPUOptions(per_process_gpu_memory_fraction=args.master_gpu_fraction))
+    main_sess = tf.Session(config=main_config)
     # set up actor agent
-    actor_agent = ActorAgent(sess, args.node_input_dim, args.job_input_dim, args.hid_dims, args.output_dim,
-                             args.max_depth, range(1, args.exec_cap + 1))
+    main_actor_agent = ActorAgent(main_sess, args.node_input_dim, args.job_input_dim, args.hid_dims, args.output_dim,
+                             args.max_depth, range(1, args.exec_cap + 1), scope='main_actor')
 
-    # collect experiences
-    while True:
-        # get parameters from master
-        (actor_params, seed, max_time, entropy_weight) = param_queue.get()
+    # tensorboard logging
+    tf_logger = TFLogger(main_sess, ['actor_loss', 'entropy', 'value_loss', 'episode_length', 'average_reward_per_second',
+                                'sum_reward', 'reset_probability', 'num_jobs', 'reset_hit', 'average_job_duration',
+                                'entropy_weight'])
 
+    # store average reward for computing differential rewards
+    avg_reward_calculator = AveragePerStepReward(args.average_reward_storage_size)
+
+    # initialize entropy parameters
+    entropy_weight = args.entropy_weight_init
+
+    # initialize episode reset probability
+    reset_prob = args.reset_prob
+
+    # ---- start training process ----
+    for ep in range(1, args.num_ep):
+        print('training epoch', ep)
+
+        # synchronize the model parameters for each training agent
+        actor_params = main_actor_agent.get_params()
+
+        # generate max time stochastically based on reset prob
+        max_time = generate_coin_flips(reset_prob)
+
+        seed = args.seed + ep
         # synchronize model
-        actor_agent.set_params(actor_params)
+        worker_actor_agent.set_params(actor_params)
 
         # reset environment
         env.seed(seed)
@@ -135,27 +167,20 @@ def train_agent(agent_id, param_queue, reward_queue, adv_queue, gradient_queue):
                'running_dag_mat': [], 'dag_summ_back_mat': [], 'node_act_vec': [], 'job_act_vec': [], \
                'node_valid_mask': [], 'job_valid_mask': [], 'reward': [], 'wall_time': [], 'job_state_change': []}
 
-        try:
-            # The masking functions (node_valid_mask and
-            # job_valid_mask in actor_agent.py) has some
-            # small chance (once in every few thousand
-            # iterations) to leave some non-zero probability
-            # mass for a masked-out action. This will
-            # trigger the check in "node_act and job_act
-            # should be valid" in actor_agent.py
-            # Whenever this is detected, we throw out the
-            # rollout of that iteration and try again.
+        # storage for advantage computation
+        all_rewards, all_diff_times, all_times, all_num_finished_jobs, all_avg_job_duration, all_reset_hit, = [], [], [], [], [], []
 
+        t1 = time.time()
+        try:
             # run experiment
             obs = env.observe()
             done = False
 
             # initial time
             exp['wall_time'].append(env.wall_time.curr_time)
-
             while not done:
 
-                node, use_exec = invoke_model(actor_agent, obs, exp)
+                node, use_exec = invoke_model(worker_actor_agent, obs, exp)
 
                 obs, reward, done = env.step(node, use_exec)
 
@@ -174,112 +199,21 @@ def train_agent(agent_id, param_queue, reward_queue, adv_queue, gradient_queue):
 
             # report reward signals to master
             assert len(exp['node_inputs']) == len(exp['reward'])
-            reward_queue.put([exp['reward'], exp['wall_time'], len(env.finished_job_dags),
+            reward_queues = [exp['reward'], exp['wall_time'], len(env.finished_job_dags),
                               np.mean([j.completion_time - j.start_time for j in env.finished_job_dags]),
-                              env.wall_time.curr_time >= env.max_time])
-
-            # get advantage term from master
-            batch_adv = adv_queue.get()
-
-            if batch_adv is None:
-                # some other agents panic for the try and the
-                # main thread throw out the rollout, reset and
-                # try again now
-                continue
-
-            # compute gradients
-            actor_gradient, loss = compute_actor_gradients(actor_agent, exp, batch_adv, entropy_weight)
-
-            # report gradient to master
-            gradient_queue.put([actor_gradient, loss])
+                              env.wall_time.curr_time >= env.max_time]
 
         except AssertionError:
-            # ask the main to abort this rollout and
-            # try again
-            reward_queue.put(None)
-            # need to still get from adv_queue to 
-            # prevent blocking
-            adv_queue.get()
+            reward_queues = None
 
-
-def main():
-    np.random.seed(args.seed)
-    tf.set_random_seed(args.seed)
-
-    # create result and model folder
-    create_folder_if_not_exists(args.result_folder)
-    create_folder_if_not_exists(args.model_folder)
-
-    # initialize communication queues
-    params_queues = [mp.Queue(1) for _ in range(args.num_agents)]
-    reward_queues = [mp.Queue(1) for _ in range(args.num_agents)]
-    adv_queues = [mp.Queue(1) for _ in range(args.num_agents)]
-    gradient_queues = [mp.Queue(1) for _ in range(args.num_agents)]
-
-    # set up training agents
-    agents = []
-    for i in range(args.num_agents):
-        agents.append(mp.Process(target=train_agent,
-                                 args=(i, params_queues[i], reward_queues[i], adv_queues[i], gradient_queues[i])))
-
-    # start training agents
-    for i in range(args.num_agents):
-        agents[i].start()
-
-    # gpu configuration
-    config = tf.ConfigProto(device_count={'GPU': args.master_num_gpu},
-                            gpu_options=tf.GPUOptions(per_process_gpu_memory_fraction=args.master_gpu_fraction))
-
-    sess = tf.Session(config=config)
-
-    # set up actor agent
-    actor_agent = ActorAgent(sess, args.node_input_dim, args.job_input_dim, args.hid_dims, args.output_dim,
-                             args.max_depth, range(1, args.exec_cap + 1))
-
-    # tensorboard logging
-    tf_logger = TFLogger(sess, ['actor_loss', 'entropy', 'value_loss', 'episode_length', 'average_reward_per_second',
-                                'sum_reward', 'reset_probability', 'num_jobs', 'reset_hit', 'average_job_duration',
-                                'entropy_weight'])
-
-    # store average reward for computing differential rewards
-    avg_reward_calculator = AveragePerStepReward(args.average_reward_storage_size)
-
-    # initialize entropy parameters
-    entropy_weight = args.entropy_weight_init
-
-    # initialize episode reset probability
-    reset_prob = args.reset_prob
-
-    # ---- start training process ----
-    for ep in range(1, args.num_ep):
-        print('training epoch', ep)
-
-        # synchronize the model parameters for each training agent
-        actor_params = actor_agent.get_params()
-
-        # generate max time stochastically based on reset prob
-        max_time = generate_coin_flips(reset_prob)
-
-        # send out parameters to training agents
-        for i in range(args.num_agents):
-            params_queues[i].put([actor_params, args.seed + ep, max_time, entropy_weight])
-
-        # storage for advantage computation
-        all_rewards, all_diff_times, all_times, all_num_finished_jobs, all_avg_job_duration, all_reset_hit, = [], [], [], [], [], []
-
-        t1 = time.time()
 
         # get reward from agents
-        any_agent_panic = False
+        result = reward_queues
 
-        for i in range(args.num_agents):
-            result = reward_queues[i].get()
-
-            if result is None:
-                any_agent_panic = True
-                continue
-            else:
-                batch_reward, batch_time, num_finished_jobs, avg_job_duration, reset_hit = result
+        if result is None:
+            continue
+        else:
+            batch_reward, batch_time, num_finished_jobs, avg_job_duration, reset_hit = result
 
             diff_time = np.array(batch_time[1:]) - np.array(batch_time[:-1])
 
@@ -295,37 +229,32 @@ def main():
         t2 = time.time()
         print('got reward from workers', t2 - t1, 'seconds')
 
-        if any_agent_panic:
-            # The try condition breaks in some agent (should
-            # happen rarely), throw out this rollout and try
-            # again for next iteration (TODO: log this event)
-            for i in range(args.num_agents):
-                adv_queues[i].put(None)
-            continue
 
         # compute differential reward
         all_cum_reward = []
         avg_per_step_reward = avg_reward_calculator.get_avg_per_step_reward()
-        for i in range(args.num_agents):
-            if args.diff_reward_enabled:
-                # differential reward mode on
-                rewards = np.array([r - avg_per_step_reward * t for (r, t) in zip(all_rewards[i], all_diff_times[i])])
-            else:
-                # regular reward
-                rewards = np.array([r for (r, t) in zip(all_rewards[i], all_diff_times[i])])
 
-            cum_reward = discount(rewards, args.gamma)
+        if args.diff_reward_enabled:
+            # differential reward mode on
+            rewards = np.array([r - avg_per_step_reward * t for (r, t) in zip(all_rewards[0], all_diff_times[0])])
+        else:
+            # regular reward
+            rewards = np.array([r for (r, t) in zip(all_rewards[0], all_diff_times[0])])
 
-            all_cum_reward.append(cum_reward)
+        cum_reward = discount(rewards, args.gamma)
+
+        all_cum_reward.append(cum_reward)
 
         # compute baseline
         baselines = get_piecewise_linear_fit_baseline(all_cum_reward, all_times)
 
         # give worker back the advantage
-        for i in range(args.num_agents):
-            batch_adv = all_cum_reward[i] - baselines[i]
-            batch_adv = np.reshape(batch_adv, [len(batch_adv), 1])
-            adv_queues[i].put(batch_adv)
+        batch_adv = all_cum_reward[0] - baselines[0]
+        batch_adv = np.reshape(batch_adv, [len(batch_adv), 1])
+
+        # compute gradients
+        actor_gradient, loss = compute_actor_gradients(worker_actor_agent, exp, batch_adv, entropy_weight)
+
 
         t3 = time.time()
         print('advantage ready', t3 - t2, 'seconds')
@@ -335,18 +264,16 @@ def main():
         all_entropy = []  # for tensorboard
         all_value_loss = []  # for tensorboard
 
-        for i in range(args.num_agents):
-            (actor_gradient, loss) = gradient_queues[i].get()
 
-            actor_gradients.append(actor_gradient)
-            all_action_loss.append(loss[0])
-            all_entropy.append(-loss[1] / float(all_cum_reward[i].shape[0]))
-            all_value_loss.append(loss[2])
+        actor_gradients.append(actor_gradient)
+        all_action_loss.append(loss[0])
+        all_entropy.append(-loss[1] / float(all_cum_reward[0].shape[0]))
+        all_value_loss.append(loss[2])
 
         t4 = time.time()
         print('worker send back gradients', t4 - t3, 'seconds')
 
-        actor_agent.apply_gradients(aggregate_gradients(actor_gradients), args.lr)
+        main_actor_agent.apply_gradients(aggregate_gradients(actor_gradients), args.lr)
 
         t5 = time.time()
         print('apply gradient', t5 - t4, 'seconds')
@@ -363,9 +290,9 @@ def main():
         reset_prob = decrease_var(reset_prob, args.reset_prob_min, args.reset_prob_decay)
 
         if ep % args.model_save_interval == 0:
-            actor_agent.save_model(args.model_folder + 'model_ep_' + str(ep))
+            main_actor_agent.save_model(args.model_folder + 'model_ep_' + str(ep))
 
-    sess.close()
+    main_sess.close()
 
 
 if __name__ == '__main__':
